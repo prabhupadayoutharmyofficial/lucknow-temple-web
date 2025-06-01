eeab2619e73c635f275a16046166e7ccb7d526b2
Saurabhchandra1024 <Saurabhchandra.uid@gmail.com>
2025-06-01 13:08:03 +0530

    hero#1

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -16,7 +16,7 @@ const Contact = () => {
         {/* Hero Section */}
         <div className="relative h-[40vh] bg-cover bg-center flex items-center justify-center" 
           style={{ 
-            backgroundImage: "url('https://images.unsplash.com/photo-1624041880741-01b13110dfd4?q=80&w=2070&auto=format&fit=crop')"
+            backgroundImage: "url('')"
           }}>
           <div className="absolute inset-0 bg-black/50"></div>
           <div className="relative text-center text-white z-10">
@@ -48,7 +48,7 @@ const Contact = () => {
                 
                 <div className="h-[400px] rounded-lg overflow-hidden shadow-lg border">
                   <iframe 
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3558.260948798046!2d80.92994757651246!3d26.89424067669716!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399bfd7dfed76d05%3A0x826a93fc6b6c57b2!2sISKCON%20Lucknow%20Vrindavan%20Dham%20-%20International%20Society%20for%20Krishna%20Consciousness!5e0!3m2!1sen!2sin!4v1735744950377!5m2!1sen!2sin" 
+                    src="" 
                     width="100%" 
                     height="100%" 
                     style={{ border: 0 }} 
